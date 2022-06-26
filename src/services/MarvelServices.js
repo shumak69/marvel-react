20ebc27ee88348e7b58f069e7c488f0265e29e0f
Dmitriy <dima06444@gmail.com>
2022-06-26 13:58:01 +0300

    first

---
+++ src/services/MarvelServices.js
@@ -0,0 +1,35 @@
+class MarvelServices {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=c3ecd3f2735edf45815f66d2ac4a27e4';
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description || 'No description',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage:  char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelServices;
